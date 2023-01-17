e2b66869734018e5a53dec29ab35e18871b61203
Muhammed Abdrahman <muhammed03amin@gmail.com>
2023-01-17 08:22:39 +0600

    feat: add implement

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import dotenv from 'dotenv';
+import { createServer } from 'http';
+import controller from "controller";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
 
-console.log(process.env.ASDF);
+export const server = createServer((req, res) => {
+    controller(req, res);
+})
+
+server.listen(+PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
